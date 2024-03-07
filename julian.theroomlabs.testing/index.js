ca4e8da90d698e252f993775b3fb69cba523b844
TheRoomLabs <156659325+TheRoomLabs@users.noreply.github.com>
2024-03-07 13:11:25 +0100

    Update index.js

--- julian.theroomlabs.testing/index.js
+++ julian.theroomlabs.testing/index.js
@@ -1,13 +1,8 @@
 const { CogsConnection, CogsVideoPlayer, CogsAudioPlayer } = COGS;
 
 const cogsConnection = new CogsConnection();
-const video = new CogsVideoPlayer(cogsConnection,);
 
 
 function pressButton(buttonName){
 	cogsConnection.sendEvent("ButtonPressed", buttonName);
-}
-
-window.onload = function(){
-	video.setParentElement(document.getElementById("center"));
 }
